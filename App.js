e353f32d8e69592efafed034fe90f4a2a8bc8df9
Jay <Nopenotj@gmail.com>
2020-05-16 18:05:16 +0800

    introduced fake data and scrollList

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { StyleSheet, View, SafeAreaView, StatusBar } from 'react-native';
+import { StyleSheet, View, SafeAreaView, StatusBar, ScrollView } from 'react-native';
 import RestaurantButton from './component/RestaurantButton';
+
+
+const fakeRestaurantData = [
+    { title : "Jerryl's topoki paradise" , descr: "best tomyum"},
+    { title : "Jays's topoki paradise" , descr: "best tomyum"},
+    { title : "Aerin's topoki paradise" , descr: "subpar tomyum"},
+    { title : "Oli's topoki paradise" , descr: "worst tomyum"},
+]
 
 const App = () => {
   return (
       <SafeAreaView style = {styles.container}>
-        <RestaurantButton 
-            name="Jerryl's Korean Restaurant"
-            description="Best ban mian ever"
-          />
+          <ScrollView>
+              { fakeRestaurantData.map((elem) => 
+              <RestaurantButton name={elem.title} description={elem.descr} 
+              />) }
+          </ ScrollView>
       </SafeAreaView>
   )
 }
