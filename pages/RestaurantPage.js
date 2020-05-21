9a96e9f20d8fd015b255482629a569e14f2aeb68
Jerryl Chong <jerrylchong@gmail.com>
2020-05-21 17:12:45 +0800

    added back button and tags for restaurant page, also edited styling for restaurant page

--- pages/RestaurantPage.js
+++ pages/RestaurantPage.js
@@ -1,15 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import {SafeAreaView, ScrollView, StatusBar, StyleSheet, View, Text, Button, Image} from "react-native";
 import DishButton from "../component/DishButton";
-import {getDishesFromApi} from "../helpers/apiHelpers";
+import {getDishesFromApi, getRestaurantTagsFromApi} from "../helpers/apiHelpers";
+import BackButton from "../component/BackButton";
+import Tag from "../component/Tag";
 
 function RestaurantPage({ navigation, route }) {
     const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
+    const [dishes, setDishes] = useState([]);
+    const [tags, setTags] = useState([]);
 
     useEffect(() => {
         Promise.all([
-            getDishesFromApi(route.params.restaurant_id).then(data => setData(data)),
+            getDishesFromApi(route.params.restaurant_id).then(data => setDishes(data)),
+            getRestaurantTagsFromApi(route.params.restaurant_id).then(data => setTags(data)),
         ])
             .catch((error) => console.error(error))
             .finally(() => setLoading(false));
@@ -17,22 +21,28 @@ function RestaurantPage({ navigation, route }) {
 
     return (
         <SafeAreaView style = {styles.container}>
-            <Button onPress = {() => navigation.goBack()} title = 'Back' />
             <Image style={styles.picture} source={require('../assets/testrestaurant.png')}/>
-
-            <View style={{display:"flex", justifyContent:"flex-start"}}>
-                <Text>Restaurant Title</Text>
-                <Text>Tags Tags Tags</Text>
-                <Text>Location : Restaurant Address</Text>
-                <Text>Opening Hours : 1200 - 1201</Text>
-                <Text>Contact No : 1200 - 1201</Text>
+            <BackButton style = {{position: 'absolute', top: 10, left: 10}} onPress = {() => navigation.goBack()} />
+            <View style={styles.header}>
+                <Text style = {styles.title}>Restaurant Title</Text>
+                <View style = {styles.tags}>
+                    { tags.map((elem, index) =>
+                        <Tag
+                            key={`tag-${index}`}
+                            name={elem.attributes.name}
+                        />
+                    ) }
+                </View>
+                <Text style = {styles.desc}>Location : Restaurant Address</Text>
+                <Text style = {styles.desc}>Opening Hours : 1200 - 1201</Text>
+                <Text style = {styles.desc}>Contact No : 1200 - 1201</Text>
             </View>
 
             <View style={{height:"10%"}} />
 
             <Text>Dishes</Text>
             <ScrollView>
-                { data.map((dish) =>
+                { dishes.map((dish) =>
                     <DishButton
                         key={dish.id}
                         title={dish.attributes.title}
@@ -61,4 +71,25 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginTop: StatusBar.currentHeight
     },
+    header: {
+        width:'100%',
+        alignItems:'flex-start',
+        padding: '2%'
+    },
+    title: {
+        fontSize: 22,
+        marginLeft: 5
+    },
+    desc: {
+        fontSize: 14,
+        color: 'grey',
+        marginLeft: 5
+    },
+    tags: {
+        flexDirection: 'row',
+        marginTop: 5,
+        alignSelf: 'flex-start',
+        marginLeft: 5,
+        marginBottom: 5
+    }
 })
