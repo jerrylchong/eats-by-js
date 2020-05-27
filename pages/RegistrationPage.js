775e74b405913a217d4bfbeaaec1835ad5bf590a
Jay <Nopenotj@gmail.com>
2020-05-27 18:16:33 +0800

    removed buggy touchablewithoutfeedback on registration page

--- pages/RegistrationPage.js
+++ pages/RegistrationPage.js
@@ -62,49 +62,47 @@ function RegistrationPage({ navigation }) {
     }
     */
     return (
-        <TouchableWithoutFeedback style={{height: "100%"}} onPress={() => Keyboard.dismiss()}>
-            <SafeAreaView style = {styles.container}>
-                <ImageBackground style = {styles.background} source={require('../assets/background.png')}/>
-                <Text style = {styles.header}>Create an Account</Text>
-                <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style = {styles.list}>
-                    <TextInput
-                        style={styles.usernameInput}
-                        placeholder="Username"
-                        onChangeText={(text) => {setUsername(text)}}
-                        value={username}
-                        textContentType='username'
-                        autoCapitalize='none'/>
-                    <TextInput
-                        style={styles.passwordInput}
-                        placeholder="Password"
-                        onChangeText={(text) => {setPassword(text)}}
-                        value={password}
-                        secureTextEntry={true}
-                        textContentType='newPassword'
-                        autoCapitalize='none'/>
-                    { isLoading && <Text>checking with db...</Text>}
-                    {
-                        !isLoading && hasErrors(errors) && Object.entries(errors).map(x => x.join(' ')).map((error,index) => 
-                            <Text
-                                key={`${index}-error`}
-                            >{error}</Text>) 
-                    }
-                </KeyboardAvoidingView>
-                <View style = {{width: '100%', height: '15%', alignItems: 'center', marginTop: '10%'}}>
-                    <View style={styles.buttonShadow}>
-                        <TouchableOpacity style={styles.button} onPress={submitHandler}>
-                            <Text style={{color: 'white'}}>Create Account</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <View style = {styles.buttons}>
-                    <Text style = {{color: '#404040'}}>Already have an account?</Text>
-                    <TouchableOpacity onPress = {() => navigation.goBack()}>
-                        <Text style = {{color:'#ffaf87', margin: '5%', fontWeight: 'bold'}}>Back</Text>
+        <SafeAreaView style = {styles.container}>
+            <ImageBackground style = {styles.background} source={require('../assets/background.png')}/>
+            <Text style = {styles.header}>Create an Account</Text>
+            <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style = {styles.list}>
+                <TextInput
+                    style={styles.usernameInput}
+                    placeholder="Username"
+                    onChangeText={(text) => {setUsername(text)}}
+                    value={username}
+                    textContentType='username'
+                    autoCapitalize='none'/>
+                <TextInput
+                    style={styles.passwordInput}
+                    placeholder="Password"
+                    onChangeText={(text) => {setPassword(text)}}
+                    value={password}
+                    secureTextEntry={true}
+                    textContentType='newPassword'
+                    autoCapitalize='none'/>
+                { isLoading && <Text>checking with db...</Text>}
+                {
+                    !isLoading && hasErrors(errors) && Object.entries(errors).map(x => x.join(' ')).map((error,index) => 
+                        <Text
+                            key={`${index}-error`}
+                        >{error}</Text>) 
+                }
+            </KeyboardAvoidingView>
+            <View style = {{width: '100%', height: '15%', alignItems: 'center', marginTop: '10%'}}>
+                <View style={styles.buttonShadow}>
+                    <TouchableOpacity style={styles.button} onPress={submitHandler}>
+                        <Text style={{color: 'white'}}>Create Account</Text>
                     </TouchableOpacity>
                 </View>
-            </SafeAreaView>
-        </TouchableWithoutFeedback>
+            </View>
+            <View style = {styles.buttons}>
+                <Text style = {{color: '#404040'}}>Already have an account?</Text>
+                <TouchableOpacity onPress = {() => navigation.goBack()}>
+                    <Text style = {{color:'#ffaf87', margin: '5%', fontWeight: 'bold'}}>Back</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     )
 }
 
