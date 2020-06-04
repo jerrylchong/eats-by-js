e02e137f5e859f99f6513135e547d9a9ff80572d
Jerryl Chong <jerrylchong@gmail.com>
2020-06-04 17:52:25 +0800

    edit tab bar color and added icons

--- pages/HomeTab.js
+++ pages/HomeTab.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { View, Image, Text, StyleSheet, Dimensions } from 'react-native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import RestaurantList from "./RestaurantList";
 import RestaurantMap from "./RestaurantMap";
@@ -8,11 +8,49 @@ const Tab = createMaterialBottomTabNavigator();
 
 function HomeTab() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="List" component={RestaurantList} />
-            <Tab.Screen name="Map" component={RestaurantMap} />
+        <Tab.Navigator
+            inactiveColor={'#ff3d33'}
+            barStyle={{backgroundColor: '#ff6961'}}
+            labeled={false}
+        >
+            <Tab.Screen name="List" component={RestaurantList}
+                        options={{
+                            tabBarIcon: ({focused, color}) =>
+                                <View style = {styles.container}>
+                                    <Image style = {styles.icon} source={require('../assets/listicon.png')}/>
+                                    <Text style = {styles.text}>List</Text>
+                                </View>,
+
+                        }}
+            />
+            <Tab.Screen name="Map" component={RestaurantMap}
+                        options={{
+                            tabBarIcon: ({focused, color}) =>
+                                <View style = {styles.container}>
+                                    <Image style = {styles.icon} source={require('../assets/mapicon.png')}/>
+                                    <Text style = {styles.text}>Map</Text>
+                                </View>
+                        }}
+            />
         </Tab.Navigator>
     );
 }
 
 export default HomeTab
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    icon: {
+        height: Dimensions.get('window').width * 0.06,
+        width: Dimensions.get('window').width * 0.06,
+    },
+    text: {
+        width:'100%',
+        fontSize: 12,
+        fontFamily: 'Ubuntu',
+        color: 'white'
+    }
+})
--- pages/RestaurantMap.js
+++ pages/RestaurantMap.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import MapView, {PROVIDER_GOOGLE} from "react-native-maps";
 import {SafeAreaView, StyleSheet, View} from "react-native";
 import SearchButton from "../container/SearchButton";
 
 const RestaurantMap = ({ navigation }) => {
+    const [searchTerm, setSearchTerm] = useState("");
+
     return (
         <SafeAreaView style = {styles.container}>
             <View style = {styles.navBar}>
-                <SearchButton navigation = {navigation}/>
+                <SearchButton
+                    navigation = {navigation}
+                    searchTerm = {searchTerm}
+                    handleSearchTerm = {searchTerm => {
+                    }}
+                />
             </View>
             <MapView
                 style={{ flex: 1, height: '90%', width: '100%' }}
